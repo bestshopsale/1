d9a0d72a825c510d06a0a194959e84f4263a9bd1
bestshopsale <38437861+bestshopsale@users.noreply.github.com>
2020-04-19 20:24:32 +0600

    1
    1

---
+++ policy.php
@@ -0,0 +1,82 @@
+
+<div>
+    <pre style="width:100%; white-space: pre-line!important;">
+ Policy on the processing of personal data
+ 1. Generalities
+This personal data processing policy is made in accordance with the requirements of the Federal law
+the law of 27.07.2006. No. 152-FZ "on personal data" and defines the procedure for processing personal data
+and measures to ensure the security of personal data data Mikhailov Ivan Sergeevich (hereinafter-the operator).
+The operator sets as its most important goal and condition for the implementation of its activities the observance of the rights and
+freedoms of a person and a citizen in the processing of their personal data, including the protection of their rights to inviolability
+privacy, personal and family secrets.
+Real the Operator's personal data processing policy (hereinafter referred to as the Policy) applies to all information,
+information that the Operator can get about website visitors <?php echo $_SERVER['SERVER_NAME']?>.
+
+Basic concepts used in Policy
+Automated processing of personal data – processing of personal data using computer technology;
+Blocking of personal data – temporary termination of processing of personal data (except in cases where
+processing is necessary to clarify personal data);
+Website – a set of graphic and informational materials, as well as computer programs and databases that provide
+their availability on the Internet at the network address <?php echo $_SERVER['SERVER_NAME']?>;
+ Personal data information system — a set of personal data contained in databases, and providing
+their processing of information technologies and technical means;
+Depersonalization of personal data — actions that result in the inability to determine without using additional
+information belonging of personal data to a specific User or other subject of personal data;
+Processing of personal data – any action (operation) or set of actions (operations) performed
+with the use of automation or without the use of such funds personal data, including collection, recording,
+systematization, accumulation, storage, refinement (update, change), extraction, use, transfer (distribution, 
+provision, access), depersonalization, blocking, deletion, destruction of personal data;
+Operator – a state authority, municipal authority, legal or natural person, independently or jointly
+with other persons who organize and / or process personal data, as well as determine the purpose of processing
+personal data, the composition of personal data to be processed, actions (operations) performed with personal data data;
+Personal data – any information related directly or indirectly to a specific or identifiable User
+the web site <?php echo $_SERVER['SERVER_NAME']?>;
+User – any visitor to the website <?php echo $_SERVER['SERVER_NAME']?>;
+ Provision of personal data – actions aimed at disclosure of personal data to a certain person
+to a person or a certain group of persons;
+Distribution of personal data – any actions aimed at disclosure of personal data to an undefined person
+to a circle of persons (transfer of personal data) or to get acquainted with personal data of an unlimited number of persons, including 
+disclosure of personal data in the mass media, placement in information and telecommunication networks 
+or providing access to personal data in any other way;
+Cross-border transfer of personal data-transfer personal data on the territory of a foreign state to the authority 
+authorities of a foreign state, a foreign individual or a foreign legal entity;
+Destruction of personal data – any actions that result in personal data being permanently destroyed with 
+the impossibility of further restoring the content of personal data in the personal data information system and 
+(or) as a result of which the material carriers of personal data are destroyed.
+
+The operator can process the following personal data of the User
+Last name, first name, patronymic;
+Email address;
+Phone number;
+Year, month, date and place of birth;
+Photo;
+Also on the website, the collection and processing of anonymous data about visitors (including "cookies") through the services of Internet statistics (Yandex Metric and Google Analytics and others).
+The above data is combined with the General concept of Personal data in the text of the Policy.
+
+Goals personal data processing
+The purpose of processing the User's personal data is to inform the User by sending e-mails; 
+providing the User with access to the services, information and/or materials contained on the website.
+The Operator also has the right to send notifications to the User about new products and services, special offers, and various events. The user can always opt out of receiving informational messages by sending An email to the operator at the policy email address@<?php echo $_SERVER['SERVER_NAME']?>  marked "Opt-out of notifications about new products and services and special offers".
+Depersonalized User data collected through Internet statistics services is used to collect information about User actions on the site, improve the quality of the site and its content.
+
+Legal grounds for processing personal data
+The operator processes the User's personal data only if they are filled in and / or sent by the User independently through special forms located on the site <?php echo $_SERVER['SERVER_NAME']?> . By filling out the appropriate forms and / or sending your personal data to the Operator, the User agrees to this Policy.
+The operator processes depersonalized data about the User if this is allowed in the User's browser settings (saving cookies and using cookies is enabled JavaScript technology).
+
+Procedure for collecting, storing, transmitting and other types of personal data processing
+The security of personal data processed by the operator is ensured by implementing legal, organizational and technical measures necessary to fully comply with the requirements of the current Legislation. legislation in the field of personal data protection.
+The operator ensures the safety of personal data and takes all possible measures to exclude access to personal data of unauthorized persons.
+The User's personal data will never, under any circumstances, be transferred to third parties, with the exception of cases related to the implementation of current legislation.
+If inaccuracies in personal data are detected, the User can update them independently by sending a notification to The operator's e-mail address of the policy Operator@<?php echo $_SERVER['SERVER_NAME']?> marked "Updating personal data".
+The period for processing personal data is unlimited. The user can withdraw his consent to the processing of personal data at any time by sending a notification to the Operator via e-mail to the e-mail address of the policy Operator@<?php echo $_SERVER['SERVER_NAME']?>  marked "Withdrawal of consent to the processing of personal data".
+
+Cross-border transfer of personal data
+The operator prior to the implementation of cross-border transfer of personal data is obliged to ensure that the foreign government on whose territory is supposed to implement the transfer of personal data, reliable protection of the rights of personal data subjects is provided.
+Cross-border transfer of personal data on the territory of foreign countries that do not meet the above requirements can only be carried out if the personal data subject has written consent to cross-border transfer personal data and / or execution of the contract to which the personal data subject is a party.
+
+Final provision
+The user can get any clarifications on issues of interest related to the processing of their personal data by contacting the Operator via e-mail policy@<?php echo $_SERVER['SERVER_NAME']?>.
+This document will reflect any changes to the Operator's personal data processing policy. The policy is valid indefinitely until it is replaced with a new version.
+ The current version of the Policy is freely available on the Internet at <?php echo $_SERVER['SERVER_NAME']?>/policy.php.
+</pre>
+</div>
